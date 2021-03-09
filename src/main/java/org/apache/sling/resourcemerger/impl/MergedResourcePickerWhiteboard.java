36dd53a8d410b7dedf465d58ff1275c7d6e8daed
Konrad Windszus <kwin@apache.org>
2021-03-09 09:34:30 +0100

    register resolverresolver registrations in map with key = service id
    add missing newline

--- src/main/java/org/apache/sling/resourcemerger/impl/MergedResourcePickerWhiteboard.java
+++ src/main/java/org/apache/sling/resourcemerger/impl/MergedResourcePickerWhiteboard.java
@@ -31,6 +31,7 @@ import org.apache.sling.resourcemerger.spi.MergedResourcePicker;
 import org.apache.sling.resourcemerger.spi.MergedResourcePicker2;
 import org.apache.sling.spi.resource.provider.ResourceProvider;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -41,13 +42,13 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 /**
  * Registers all {@link MergedResourcePicker} and {@link MergedResourcePicker2} services as {@link MergingResourceProvider}s.
  */
+@SuppressWarnings("deprecation")
 @Component
 public class MergedResourcePickerWhiteboard {
 
-    
     private BundleContext bundleContext;
 
-    private final Map<String, ServiceRegistration<ResourceProvider>> resourceProvidersPerRoot = new ConcurrentHashMap<>();
+    private final Map<Long, ServiceRegistration<ResourceProvider<Void>>> resourceProvidersPerPickerServiceId = new ConcurrentHashMap<>();
 
     @Activate
     protected void activate(final BundleContext bundleContext) {
@@ -56,13 +57,13 @@ public class MergedResourcePickerWhiteboard {
 
     @Deactivate
     protected void deactivate() {
-        for (ServiceRegistration<ResourceProvider> resourceProvider : resourceProvidersPerRoot.values()) {
+        for (ServiceRegistration<ResourceProvider<Void>> resourceProvider : resourceProvidersPerPickerServiceId.values()) {
             resourceProvider.unregister();
         }
     }
 
     @Reference(policy = ReferencePolicy.DYNAMIC)
-    public void bindMergedResourcePicker(MergedResourcePicker resourcePicker, Map<String, String> properties) {
+    public void bindMergedResourcePicker(MergedResourcePicker resourcePicker, Map<String, Object> properties) {
         final MergedResourcePicker2 resourcePicker2 = new MergedResourcePicker2() {
 
             @Override
@@ -73,44 +74,44 @@ public class MergedResourcePickerWhiteboard {
         registerMergingResourceProvider(resourcePicker2, properties);
     }
 
-    public void unbindMergedResourcePicker(Map<String, String> properties) {
+    public void unbindMergedResourcePicker(Map<String, Object> properties) {
         unregisterMergingResourceProvider(properties);
     }
 
     @Reference(policy = ReferencePolicy.DYNAMIC)
-    public void bindMergedResourcePicker2(MergedResourcePicker2 resourcePicker, Map<String, String> properties) {
+    public void bindMergedResourcePicker2(MergedResourcePicker2 resourcePicker, Map<String, Object> properties) {
         registerMergingResourceProvider(resourcePicker, properties);
     }
 
-    public void unbindMergedResourcePicker2(Map<String, String> properties) {
+    public void unbindMergedResourcePicker2(Map<String, Object> properties) {
         unregisterMergingResourceProvider(properties);
     }
 
-    private void registerMergingResourceProvider(MergedResourcePicker2 resourcePicker, Map<String, String> properties) {
-        final String mergeRoot = properties.getOrDefault(MergedResourcePicker2.MERGE_ROOT, null);
+    @SuppressWarnings("unchecked")
+    private void registerMergingResourceProvider(MergedResourcePicker2 resourcePicker, Map<String, Object> properties) {
+        final String mergeRoot = (String)properties.getOrDefault(MergedResourcePicker2.MERGE_ROOT, null);
         if (mergeRoot != null) {
             boolean readOnly = PropertiesUtil.toBoolean(properties.get(MergedResourcePicker2.READ_ONLY), true);
             boolean traverseParent = PropertiesUtil.toBoolean(properties.get(MergedResourcePicker2.TRAVERSE_PARENT), false);
 
-            MergingResourceProvider provider = readOnly ?
+            ResourceProvider<Void> provider = readOnly ?
                     new MergingResourceProvider(mergeRoot, resourcePicker, true, traverseParent) :
                     new CRUDMergingResourceProvider(mergeRoot, resourcePicker, traverseParent);
-    
             final Dictionary<String, Object> props = new Hashtable<>();
             props.put(ResourceProvider.PROPERTY_NAME, readOnly ? "Merging" : "CRUDMerging");
             props.put(ResourceProvider.PROPERTY_ROOT, mergeRoot);
             props.put(ResourceProvider.PROPERTY_MODIFIABLE, !readOnly);
             props.put(ResourceProvider.PROPERTY_AUTHENTICATE, ResourceProvider.AUTHENTICATE_NO);
-    
-            final ServiceRegistration<ResourceProvider> resourceProvider = bundleContext.registerService(ResourceProvider.class, provider, props);
-            resourceProvidersPerRoot.put(mergeRoot, resourceProvider);
+            final Long key = (Long) properties.get(Constants.SERVICE_ID);
+            final ServiceRegistration<ResourceProvider<Void>> resourceProvider = (ServiceRegistration<ResourceProvider<Void>>)bundleContext.registerService(ResourceProvider.class.getName(), provider, props);
+            resourceProvidersPerPickerServiceId.put(key, resourceProvider);
         }
     }
 
-    private void unregisterMergingResourceProvider(Map<String, String> properties) {
-        final String mergeRoot = properties.getOrDefault(MergedResourcePicker2.MERGE_ROOT, null);
-        if (mergeRoot != null) {
-            final ServiceRegistration<ResourceProvider> resourceProvider = resourceProvidersPerRoot.get(mergeRoot);
+    private void unregisterMergingResourceProvider(Map<String, Object> properties) {
+        final Long key = (Long) properties.get(Constants.SERVICE_ID);
+        if (key != null) {
+            final ServiceRegistration<ResourceProvider<Void>> resourceProvider = resourceProvidersPerPickerServiceId.get(key);
             if (resourceProvider != null) {
                 resourceProvider.unregister();
             }
